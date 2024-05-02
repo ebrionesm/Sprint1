03a1f337b729150f0a2144b36a37d8bff63abc7b
Edu <ebrionesm96@gmail.com>
2024-05-02 13:06:21 +0200

    Tema2_Ex1

---
+++ Tema2/index.php
@@ -0,0 +1,13 @@
+<?php
+    $int = 0;
+    $double = 0.5;
+    $str = "String";
+    $bool = true;
+    define("nombre", "Edu");
+
+    echo $int;
+    echo $double;
+    echo $str;
+    echo $bool;
+    echo nombre;
+?>
