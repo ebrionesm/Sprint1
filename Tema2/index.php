be08f295e60d29315d1f102428e96a6079956141
Edu <ebrionesm96@gmail.com>
2024-05-02 13:48:45 +0200

    Tema2_MitadEx3

--- Tema2/index.php
+++ Tema2/index.php
@@ -6,20 +6,42 @@
     
     define("nombre", "Eduardo");
 
-    echo $int;
-    echo $double;
-    echo $str;
-    echo $bool;
-    echo nombre;
+    echo $int . "<br>";
+    echo $double . "<br>";
+    echo $str . "<br>";
+    echo $bool . "<br>";
+    echo nombre . "<br>";
 
     $helloWorld = "Hello World!";
-    echo $helloWorld;
+    echo $helloWorld . "<br>";
     
-    echo strtoupper($helloWorld);
-    echo strlen($helloWorld);
-    echo strrev($helloWorld);
+    echo strtoupper($helloWorld) . "<br>";
+    echo strlen($helloWorld) . "<br>";
+    echo strrev($helloWorld) . "<br>";
 
     $cursPhpStr = "Aquest és el curs PHP";
-    echo $helloWorld . ' ' . $cursPhpStr;
+    echo $helloWorld . ' ' . $cursPhpStr . "<br>";
+
+    $X = 4;
+    $Y = 7;
+    $N = 3.5;
+    $M = 2.3;
+
+    echo $X . "<br>";
+    echo $Y . "<br>";
+    echo 'Suma: ' . $X+$Y . "<br>";
+    echo 'Resta: ' . $X-$Y . "<br>";
+    echo 'Producto: ' . $X*$Y . "<br>";
+    echo 'Módulo: ' . $X/$Y . "<br>";
+
+    echo $N . "<br>";
+    echo $M . "<br>";
+    echo 'Suma: ' . $N+$M . "<br>";
+    echo 'Resta: ' . $N-$M . "<br>";
+    echo 'Producto: ' . $N*$M . "<br>";
+    echo 'Módulo: ' . $N/$M . "<br>";
+
+    echo "X*2: " . $X*2;
+
 
 ?>
