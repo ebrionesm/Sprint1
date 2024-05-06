c349e276954d765a9db3524e3aee2f5da993692d
Edu <ebrionesm96@gmail.com>
2024-05-06 10:56:20 +0200

    Tema2_Ex4_Actualizado

--- Tema2/ex4.php
+++ Tema2/ex4.php
@@ -1,12 +1,12 @@
 <?php
-    $numeroFinal = 8;
-    $intervalo = 1;
+    $numeroFinal = 15;
+    $intervalo = 3;
 
     contador($numeroFinal, $intervalo);
 
     function contador($numeroFinal = 10, $intervalo)
     {
-        $numero = 0;
+        echo "Número final: " . $numeroFinal . " . Intervalo: " . $intervalo . "<br>";
         for($i = 0; $i <= $numeroFinal; $i += $intervalo)
         {
             echo $i . "<br>";
